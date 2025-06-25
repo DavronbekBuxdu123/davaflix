05283a1817a6b2c71d4afda8261f706540827120
Aslonov Davronbek <davronaslonov01@gmail.com>
2025-06-25 14:37:46 +0500

    update project

--- navigations/appNavigation.tsx
+++ navigations/appNavigation.tsx
@@ -2,32 +2,35 @@ import Home from "@/screens/Home";
 import Movie from "@/screens/Movie";
 import Person from "@/screens/Person";
 import Search from "@/screens/Search";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 const Stack = createNativeStackNavigator();
 export default function AppNavigations() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="Movie"
-        component={Movie}
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="Person"
-        component={Person}
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="Search"
-        component={Search}
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Movie"
+          component={Movie}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Person"
+          component={Person}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Search"
+          component={Search}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
